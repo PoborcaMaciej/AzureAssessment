af07a374c7c5d62137f0a89b5ab5b923af1f1d75
Maciej Poborca <poborcamaciej@gmail.com>
2022-02-23 14:50:45 +0100

    Warning preference set to SilentlyContinue

--- Get-AzureAssessment.ps1
+++ Get-AzureAssessment.ps1
@@ -12,16 +12,29 @@ V1.00, 15/02/2022 - Initial version (Subscription and VM assessment)
 V1.01, 23/02/2022 - Allow to execute directly from cloud shell
 #>
 <# Declaring classes #>
-
+$WarningPreference = 'SilentlyContinue'
 Write-Output "$(Get-Date -format HH:mm:ss)  |  Executing Subscription report"
-Invoke-WebRequest -Uri https://raw.githubusercontent.com/PoborcaMaciej/AzureAssessment/main/Get-AzureSubscriptions.ps1 -OutFile 'Get-AzureSubscriptions.ps1'
+if ( ! $(Get-Item -path Get-AzureSubscriptions.ps1) )
+{
+    Invoke-WebRequest -Uri https://raw.githubusercontent.com/PoborcaMaciej/AzureAssessment/main/Get-AzureSubscriptions.ps1 -OutFile 'Get-AzureSubscriptions.ps1'
+}
 .\Get-AzureSubscriptions.ps1
 Write-Output "$(Get-Date -format HH:mm:ss)  |  Executing VM Report"
-Invoke-WebRequest -Uri https://raw.githubusercontent.com/PoborcaMaciej/AzureAssessment/main/Get-AzureVMs.ps1 -OutFile 'Get-AzureVMs.ps1'
+if ( ! $(Get-Item -path Get-AzureVMs.ps1) )
+{
+    Invoke-WebRequest -Uri https://raw.githubusercontent.com/PoborcaMaciej/AzureAssessment/main/Get-AzureVMs.ps1 -OutFile 'Get-AzureVMs.ps1'
+}
 .\Get-AzureVMs.ps1
 Write-Output "$(Get-Date -format HH:mm:ss)  |  Executing Defender for cloud recommendations report"
-Invoke-WebRequest -Uri https://raw.githubusercontent.com/PoborcaMaciej/AzureAssessment/main/get-DefenderRecommendations.ps1 -OutFile 'Get-DefenderRecommendations.ps1'
+if ( ! $(Get-Item -path Get-DefenderRecommendations.ps1) )
+{
+    Invoke-WebRequest -Uri https://raw.githubusercontent.com/PoborcaMaciej/AzureAssessment/main/get-DefenderRecommendations.ps1 -OutFile 'Get-DefenderRecommendations.ps1'
+}
 .\Get-DefenderRecommendations.ps1
 Write-Output "$(Get-Date -format HH:mm:ss)  |  Executing Defender for cloud assessment report"
-Invoke-WebRequest -Uri https://raw.githubusercontent.com/PoborcaMaciej/AzureAssessment/main/get-azuredefenderassessment.ps1 -OutFile 'get-azuredefenderassessment.ps1'
+Write-Output "$(Get-Date -format HH:mm:ss)  |  Executing Defender for cloud recommendations report"
+if ( ! $(Get-Item -path get-azuredefenderassessment.ps1) )
+{
+    Invoke-WebRequest -Uri https://raw.githubusercontent.com/PoborcaMaciej/AzureAssessment/main/get-azuredefenderassessment.ps1 -OutFile 'get-azuredefenderassessment.ps1'
+}
 .\get-azuredefenderassessment.ps1
