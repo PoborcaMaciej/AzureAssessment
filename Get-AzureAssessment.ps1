5918e355ba4c882dff0176c0fc1d89769ef2e895
Maciej Poborca <poborcamaciej@gmail.com>
2022-02-28 12:01:49 +0100

    revert

--- Get-AzureAssessment.ps1
+++ Get-AzureAssessment.ps1
@@ -14,14 +14,14 @@ V1.01, 23/02/2022 - Allow to execute directly from cloud shell
 <# Declaring classes #>
 $WarningPreference = 'SilentlyContinue'
 Write-Output "$(Get-Date -format HH:mm:ss)  |  Executing Subscription report"
-Invoke-WebRequest -Uri https://raw.githubusercontent.com/PoborcaMaciej/AzureAssessment/main/Get-AzureSubscriptions.ps1?token=GHSAT0AAAAAABRTBV2ZORM3LAYNGWZE6MEGYQ4U7LQ -OutFile 'Get-AzureSubscriptions.ps1'
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/PoborcaMaciej/AzureAssessment/main/Get-AzureSubscriptions.ps1 -OutFile 'Get-AzureSubscriptions.ps1'
 .\Get-AzureSubscriptions.ps1
 Write-Output "$(Get-Date -format HH:mm:ss)  |  Executing VM Report"
-Invoke-WebRequest -Uri https://raw.githubusercontent.com/PoborcaMaciej/AzureAssessment/main/Get-AzureVMs.ps1?token=GHSAT0AAAAAABRTBV2Z25ZOPPSJSEO4WFNUYQ4U7SQ -OutFile 'Get-AzureVMs.ps1'
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/PoborcaMaciej/AzureAssessment/main/Get-AzureVMs.ps1 -OutFile 'Get-AzureVMs.ps1'
 .\Get-AzureVMs.ps1
 Write-Output "$(Get-Date -format HH:mm:ss)  |  Executing Defender for cloud recommendations report"
-Invoke-WebRequest -Uri https://raw.githubusercontent.com/PoborcaMaciej/AzureAssessment/main/get-DefenderRecommendations.ps1?token=GHSAT0AAAAAABRTBV2YGNOHZTPSYSN4LP3IYQ4U73Q -OutFile 'Get-DefenderRecommendations.ps1'
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/PoborcaMaciej/AzureAssessment/main/get-DefenderRecommendations.ps1 -OutFile 'Get-DefenderRecommendations.ps1'
 .\Get-DefenderRecommendations.ps1
 Write-Output "$(Get-Date -format HH:mm:ss)  |  Executing Defender for cloud assessment report"
-Invoke-WebRequest -Uri https://raw.githubusercontent.com/PoborcaMaciej/AzureAssessment/main/get-azuredefenderassessment.ps1?token=GHSAT0AAAAAABRTBV2Y4AMEIZSE6NAMG5O6YQ4VAGA -OutFile 'get-azuredefenderassessment.ps1'
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/PoborcaMaciej/AzureAssessment/main/get-azuredefenderassessment.ps1 -OutFile 'get-azuredefenderassessment.ps1'
 .\get-azuredefenderassessment.ps1
