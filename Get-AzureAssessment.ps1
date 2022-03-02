fd68ca5972e24efcb10123d517bc56b32a7bf239
Maciej Poborca <poborcamaciej@gmail.com>
2022-03-02 16:13:38 +0100

    Azure Consumption report

--- Get-AzureAssessment.ps1
+++ Get-AzureAssessment.ps1
@@ -10,7 +10,7 @@ Website:	https://maciejpoborca.pl
 Change Log
 V1.00, 15/02/2022 - Initial version (Subscription and VM assessment)
 V1.01, 23/02/2022 - Allow to execute directly from cloud shell
-v1.02, 02/03/2022 - Add Azure Advisor Recommendation + Information where to find fileshare
+v1.02, 02/03/2022 - Add Azure Advisor Recommendation + Information where to find fileshare + Azure Consumption report
 #>
 <# Declaring classes #>
 $WarningPreference = 'SilentlyContinue'
@@ -26,10 +26,15 @@ Invoke-WebRequest -Uri https://raw.githubusercontent.com/PoborcaMaciej/AzureAsse
 Write-Output "$(Get-Date -format HH:mm:ss)  |  Executing Defender for cloud assessment report"
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/PoborcaMaciej/AzureAssessment/main/get-azuredefenderassessment.ps1 -OutFile 'get-azuredefenderassessment.ps1'
 .\get-azuredefenderassessment.ps1
-Write-Output "$(Get-Date -format HH:mm:ss)  |  Executing Defender for cloud Azure Advisor Recommendations"
+Write-Output "$(Get-Date -format HH:mm:ss)  |  Executing Azure Advisor Recommendations report"
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/PoborcaMaciej/AzureAssessment/main/get-AzureAdvisorRecommendations.ps1 -OutFile 'get-azureAdvisorRecommendations.ps1'
 .\get-azureAdvisorRecommendations.ps1
+Write-Output "$(Get-Date -format HH:mm:ss)  |  Executing Azure Consumption report"
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/PoborcaMaciej/AzureAssessment/main/get-azureconsumptiondetails.ps1 -OutFile 'get-azureconsumptiondetails.ps1'
+.\get-azureconsumptiondetails.ps1
 
-Write-Output $('- Your report is completed' )
+
+
+Write-Output $('- Your reports are completed and stored in the fileshare shown below' )
 Write-Output $('   Storage Account: ' + $(Get-CloudDrive).Name )
 Write-Output $('    FileShare Name: ' + $(Get-CloudDrive).FileShareName )
